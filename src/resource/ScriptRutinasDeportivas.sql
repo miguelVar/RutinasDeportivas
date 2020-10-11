f99990601c235ddd9cb0f68855c6122ee07d4386
miguelVar <vargas.miguel32@gmail.com>
2020-10-10 22:30:33 -0500

    se agrega el actualizar

--- src/resource/ScriptRutinasDeportivas.sql
+++ src/resource/ScriptRutinasDeportivas.sql
@@ -1,68 +1,12 @@
---==============================================================
--- DBMS name:      ANSI Level 2
--- Created on:     3/10/2020 10:23:53 p. m.
---==============================================================
-
-
-drop index RELACION_TIPO_FK;
-
-drop index EJERCICIO_PK;
-
-drop table EJERCICIO cascade;
-
-drop index RELACION_REGISTRO_FK;
-
-drop index ESPECIALISTA_PK;
-
-drop table ESPECIALISTA cascade;
-
-drop index RELACION_AMIGOS_FK;
-
-drop index RELACION_ESPECIALISTA_FK;
-
-drop index GRUPO_DEPORTIVO_PK;
-
-drop table GRUPO_DEPORTIVO cascade;
-
-drop index RELACION_MUSCULO_EJERCICIO_FK;
-
-drop index RELACION_EJERCICIO_MUSCULO_FK;
-
-drop index MUSCULO_EJERCICIO_PK;
-
-drop table MUSCULO_EJERCICIO cascade;
-
-drop index RELACION_PARTE_CUERPO_FK;
-
-drop index PARTE_CUERPO_PK;
-
-drop table PARTE_CUERPO cascade;
-
-drop index RELACION_REGISTRO2_FK;
-
-drop index REGISTRO_PK;
-
-drop table REGISTRO cascade;
-
-drop index RUTINA_ESPECIALISTA_FK;
-
-drop index RUTINA_EJERCICIO_FK;
-
-drop index RUTINA_PK;
-
-drop table RUTINA cascade;
-
-drop index TIPO_EJERCICIO_PK;
-
-drop table TIPO_EJERCICIO cascade;
-
+DROP DATABASE IF EXISTS Rutinas;
+CREATE DATABASE Rutinas;
+USE Rutinas;
 
 create table TIPO_EJERCICIO (
 ID_TIPO_EJERCICIO    SMALLINT             not null,
 DSC_TIPO_EJERCICIO   VARCHAR(50),
 primary key (ID_TIPO_EJERCICIO)
 );
-
 
 create table EJERCICIO (
 ID_EJERCICIO         INTEGER              not null,
@@ -74,7 +18,6 @@ foreign key (ID_TIPO_EJERCICIO)
       references TIPO_EJERCICIO (ID_TIPO_EJERCICIO)
 );
 
-
 create table ESPECIALISTA (
 ID_ESPECIALISTA      INTEGER              not null,
 NOMBRE               VARCHAR(50)          not null,
@@ -82,7 +25,6 @@ FECHA_NACIMIENTO     DATE                 not null,
 TARJETA_PROFESIONAL  VARCHAR(12)          not null,
 primary key (ID_ESPECIALISTA)
 );
-
 
 create table GRUPO_DEPORTIVO (
 CSC_INTENTOS         INTEGER              not null,
@@ -125,7 +67,6 @@ primary key (ID_REGISTRO),
 foreign key (ID_REGISTRO)
       references ESPECIALISTA (ID_ESPECIALISTA)
 );
-
 
 create table RUTINA (
 ID_ESPECIALISTA      INTEGER              not null,
